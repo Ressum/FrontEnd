96f5eeb663d75ff9c81e861727de32a0f2646aab
kangbinkwon <success200655@naver.com>
2022-09-15 01:13:41 +0900

    FEAT :: 책 구매 페이지 제작

--- src/components/Main/Payment/Payment.js
+++ src/components/Main/Payment/Payment.js
@@ -1,6 +1,110 @@
+import './Payment.scss';
+import bookImg from 'images/Payment/book.svg';
+
 function Payment() {
     return(
-        <div>Payment</div>
+        <div id="payment">
+            <h1>주문 / 결제</h1>
+            <div id="payment-line"></div>
+            <form>
+                <div id="purchaser-info">
+                    <h2>구매자 정보</h2>
+                    <ul>
+                        <li>
+                            <h3>이름</h3>
+                            <div></div>
+                        </li>
+                        <li>
+                            <h3>이메일</h3>
+                            <div></div>
+                        </li>
+                        <li>
+                            <h3>휴대폰 번호</h3>
+                            <div></div>
+                        </li>
+                    </ul>
+                </div>
+                <div id="receiver-info">
+                    <h2>받는 사람 정보</h2>
+                    <ul>
+                        <li>
+                            <h3>이름</h3>
+                            <div></div>
+                        </li>
+                        <li>
+                            <h3>배송 주소</h3>
+                            <div></div>
+                        </li>
+                        <li>
+                            <h3>연락처</h3>
+                            <div></div>
+                        </li>
+                    </ul>
+                </div>
+                <div id="purchase-book-type">
+                    <label id="common-book">
+                        <div>
+                            <div>
+                                <h3>일반 책 구매</h3>
+                                <div>30000₩</div>
+                            </div>
+                            <input type="radio" name="book-type" />
+                        </div>
+                        <img src={bookImg} alt="book" />
+                    </label>
+                    <label id="used-book">
+                        <div>
+                            <div>
+                                <h3>중고 책 구매</h3>
+                                <div>13000₩</div>
+                            </div>
+                            <input type="radio" name="book-type" />
+                        </div>
+                        <img src={bookImg} alt="book" />
+                    </label>
+                    <div id="monthly-book-purchases">
+                        <div>
+                            <h3>월간 책 구매 건수</h3>
+                            <select>
+                                <option value="1" defaultValue>1권</option>
+                                <option value="2">2권</option>
+                                <option value="3">3권</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div id="payment-info">
+                    <h2>결제 정보</h2>
+                    <ul>
+                        <li>
+                            <h3>총상품가격</h3>
+                            <div></div>
+                        </li>
+                        <li>
+                            <h3>할인 쿠폰</h3>
+                            <div></div>
+                        </li>
+                        <li>
+                            <h3>배송비</h3>
+                            <div></div>
+                        </li>
+                        <li>
+                            <h3>래썸캐시</h3>
+                            <div></div>
+                        </li>
+                        <li>
+                            <h3>총 결제금액</h3>
+                            <div></div>
+                        </li>
+                        <li>
+                            <h3>결제방법</h3>
+                            <div></div>
+                        </li>
+                    </ul>
+                </div>
+                <button>결제</button>
+            </form>
+        </div>
     );
 }
 
