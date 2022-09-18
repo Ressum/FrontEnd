1307de7fe810a534fcedff0ef6b9e32f2dbc80b2
kangbinkwon <success200655@naver.com>
2022-09-19 03:07:10 +0900

    FEAT :: 출석체크 페이지 제작

--- src/components/Main/AttendanceCheck/AttendanceCheck.js
+++ src/components/Main/AttendanceCheck/AttendanceCheck.js
@@ -1,6 +1,38 @@
+import { useRef } from 'react';
+import './AttendanceCheck.scss';
+
 function AttendanceCheck() {
+    const days = useRef(['', '', '', 1, 2, 3, 4, 
+        5, 6, 7, 8, 9, 10, 11, 
+        12, 13, 14, 15, 16, 17, 18, 
+        19, 20, 21, 22, 23, 24, 25, 
+        26, 27, 28, 29, 30, '', '']);
     return(
-        <div>AttendanceCheck</div>
+        <div id="attendancecheck">
+            <h1>출석체크</h1>
+            <div id="attendancecheck-line"></div>
+            <div id="calendar">
+                <div id="dates">
+                    <div>Mon</div>
+                    <div>Tue</div>
+                    <div>Wed</div>
+                    <div>Thu</div>
+                    <div>Fri</div>
+                    <div>Sat</div>
+                    <div>Sun</div>
+                </div>
+                <ul id="days">
+                    {
+                        days.current.map((day, index) => (
+                            <li key={index}>
+                                {day}
+                            </li>
+                        ))
+                    }
+                </ul>
+                <button>출석체크하기</button>
+            </div>
+        </div>
     );
 }
 
