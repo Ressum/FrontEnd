86fb86095312c7512698b540de4d5b58f4d0fb2e
arkk200 <arkk20000@gmail.com>
2022-09-30 23:20:12 +0900

    FIX :: 헤더에 라우터 설정 코드 수정

--- src/components/Main/Header/Header.js
+++ src/components/Main/Header/Header.js
@@ -3,48 +3,60 @@ import homeIcon from 'images/Header/home.svg';
 import partyPopperIcon from 'images/Header/party-popper.svg';
 import calenderIcon from 'images/Header/calender.svg';
 import writeIcon from 'images/Header/write.svg';
+import { Link } from 'react-router-dom';
 function Header() {
-    function home(){
-        window.location.href='../main/Home';
+    function home() {
+        window.location.href = '../main/Home';
     }
-    function party(){
-        window.location.href='../main/Event';
+    function party() {
+        window.location.href = '../main/Event';
     }
-    function notice(){
-        window.location.href='../main/notice';
+    function notice() {
+        window.location.href = '../main/notice';
     }
-    function write(){
-        window.location.href='../main/write';
+    function write() {
+        window.location.href = '../main/write';
     }
-    function mypages(){
-        window.location.href='../main/mypage';
+    function mypages() {
+        window.location.href = '../main/mypage';
     }
-    return(
+    return (
         <header>
-            <div id="logo" onClick={home}>
-                <h1>Rasham</h1>
-                <h6>맞춤형 독서 교육 플랫폼</h6>
+            <div id="logo"> {/** */}
+                <Link to="/main/home">
+                    <h1>Rasham</h1>
+                    <h6>맞춤형 독서 교육 플랫폼</h6>
+                </Link>
             </div>
             <ul>
-                <li onClick={home}>
-                    <img src={homeIcon} alt="home" className="notice"/>
-                    홈
+                <li> {/** */}
+                    <Link to="/main/home">
+                        <img src={homeIcon} alt="home" className="notice" />
+                        홈
+                    </Link>
                 </li>
-                <li onClick={party}>
-                    <img src={partyPopperIcon} alt="event" />
-                    이벤트
+                <li> {/** */}
+                    <Link to="/main/event">
+                        <img src={partyPopperIcon} alt="event" />
+                        이벤트
+                    </Link>
                 </li>
-                <li onClick={notice}>
-                    <img src={calenderIcon} alt="notice" className="notice"/>
-                    공지사항
+                <li> {/** */}
+                    <Link to="/main/notice">
+                        <img src={calenderIcon} alt="notice" className="notice" />
+                        공지사항
+                    </Link>
                 </li>
-                <li onClick={write}>
-                    <img src={writeIcon} alt="write" className="write" />
-                    글쓰기
-                </li>
-            </ul>
-            <div id="header-profile-image" onClick={mypages}>
-                <img src="" alt="profile" />
+                <li> {/** */}
+                    <Link to="/main/write">
+                        <img src={writeIcon} alt="write" className="write" />
+                        글쓰기
+                    </Link>
+                </li>            </ul>
+            <div id="header-profile-image"> {/** */}
+                <Link to="/main/mypage">
+                    <img src="" alt="profile" />
+                </Link>
             </div>
         </header>
     );
