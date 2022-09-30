b6d9a5dfa45725fa89d98baa9ebb73c6f3c8eb48
kangbinkwon <kbk282655@gmail.com>
2022-10-01 00:15:46 +0900

    CHORE :: 헤더바 라우팅 완료

--- src/components/Main/Header/Header.js
+++ src/components/Main/Header/Header.js
@@ -3,49 +3,35 @@ import homeIcon from 'images/Header/home.svg';
 import partyPopperIcon from 'images/Header/party-popper.svg';
 import calenderIcon from 'images/Header/calender.svg';
 import writeIcon from 'images/Header/write.svg';
+import { Link } from 'react-router-dom';
 function Header() {
-    function home(){
-        window.location.href='../main/Home';
-    }
-    function party(){
-        window.location.href='../main/Event';
-    }
-    function notice(){
-        window.location.href='../main/notice';
-    }
-    function write(){
-        window.location.href='../main/write';
-    }
-    function mypages(){
-        window.location.href='../main/mypage';
-    }
     return(
         <header>
-            <div id="logo" onClick={home}>
+            <Link id="logo" to='/main/Home'>
                 <h1>Rasham</h1>
                 <h6>맞춤형 독서 교육 플랫폼</h6>
-            </div>
+            </Link>
             <ul>
-                <li onClick={home}>
-                    <img src={homeIcon} alt="home" className="notice"/>
-                    홈
-                </li>
-                <li onClick={party}>
+                <Link to='/main/Home'>
+                        <img src={homeIcon} alt="home" className="notice"/>
+                        홈
+                </Link>
+                <Link to='/main/Event'>
                     <img src={partyPopperIcon} alt="event" />
                     이벤트
-                </li>
-                <li onClick={notice}>
+                </Link>
+                <Link to='/main/notice'>
                     <img src={calenderIcon} alt="notice" className="notice"/>
                     공지사항
-                </li>
-                <li onClick={write}>
+                </Link>
+                <Link to='/main/write'>
                     <img src={writeIcon} alt="write" className="write" />
                     글쓰기
-                </li>
+                </Link>
             </ul>
-            <div id="header-profile-image" onClick={mypages}>
-                <img src="" alt="profile" />
-            </div>
+            <Link id="header-profile-image" to='/main/mypage'>
+                    <img src="" alt="profile" />
+            </Link>
         </header>
     );
 }
