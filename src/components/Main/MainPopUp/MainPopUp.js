057c0bbc8ab314040570b1b6438e2a713235cde5
arkk200 <arkk20000@gmail.com>
2022-10-09 20:53:06 +0900

    CHORE :: 메인 팝업 제작

--- src/components/Main/MainPopUp/MainPopUp.js
+++ src/components/Main/MainPopUp/MainPopUp.js
@@ -1,6 +1,20 @@
-function MainPopUp() {
+import './MainPopUp.scss';
+
+function MainPopUp({ setShowPopUp }) {
     return (
-        <div></div>
+        <div id="main-pop-up-bg">
+            <div id="main-pop-up">
+                <h2>타이틀 제목</h2>
+                <div id="main-pop-up-contents">ㅓㄴ마ㅣㅁ나ㅣ어닝ㄴ민ㅁㄴㅇ
+ㅁㄴ우ㅏㅣㄴ무인무이니위ㅣ</div>
+                <div id="main-pop-up-buttons">
+                    <button id="close-button" onClick={() => setShowPopUp(false)}>닫기</button>
+                    <button id="short-cut-button">바로가기</button>
+                </div>
+                <div id="do-not-show" onClick={() => setShowPopUp(false)}>일주일 동안 보지 않기</div>
+                <div id="main-pop-up-line"></div>
+            </div>
+        </div>
     )
 }
 
