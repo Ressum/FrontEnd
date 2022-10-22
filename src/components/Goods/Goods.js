06ca457e3585bd5b34db2ebd6a2eff4aaf2496d9
arkk200 <arkk20000@gmail.com>
2022-10-22 15:02:11 +0900

    CHORE :: Goods 페이지 CSS 추가

--- src/components/Goods/Goods.js
+++ src/components/Goods/Goods.js
@@ -1,13 +1,20 @@
 import GoodsData from './Goods.json';
+import './Goods.scss';
+import ShoppingBag from 'images/Goods/shopping-bag.svg';
+import Profile from 'images/Goods/profile.svg';
 
 function Goods() {
     return(
         <div id="goods">
             <div id="goods-header">
-                <div id="goods-logo"></div>
+                <div id="goods-logo">
+                    Rasham<br />
+                    Goods<br />
+                    Shop
+                </div>
                 <div id="goods-user-info">
-                    <div id="goods-shopping-bag"></div>
-                    <div id="goods-profile"></div>
+                    <img id="goods-shopping-bag" src={ShoppingBag} />
+                    <img id="goods-profile" src={Profile} />
                 </div>
             </div>
             <div id="goods-title">Items</div>
@@ -17,7 +24,7 @@ function Goods() {
                         <li key={goodsInfo.goodsId}>
                             <img src={goodsInfo.thumbnailUrl} />
                             <h6>{goodsInfo.goodsName}</h6>
-                            <div>{goodsInfo.goodsPrice}</div>
+                            <div>{goodsInfo.goodsPrice}₩</div>
                         </li>
                     ))
                 }
