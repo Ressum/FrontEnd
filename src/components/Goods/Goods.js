6b5010c0d31819753a2b2e4649c6a96bfa293c38
arkk200 <arkk20000@gmail.com>
2022-10-22 14:25:39 +0900

    FEAT :: Goods 페이지 형식 제작

--- src/components/Goods/Goods.js
+++ src/components/Goods/Goods.js
@@ -1,6 +1,28 @@
+import GoodsData from './Goods.json';
+
 function Goods() {
     return(
-        <div>Goods</div>
+        <div id="goods">
+            <div id="goods-header">
+                <div id="goods-logo"></div>
+                <div id="goods-user-info">
+                    <div id="goods-shopping-bag"></div>
+                    <div id="goods-profile"></div>
+                </div>
+            </div>
+            <div id="goods-title">Items</div>
+            <ul id="goods-list">
+                {
+                    GoodsData.goodsList.map(goodsInfo => (
+                        <li key={goodsInfo.goodsId}>
+                            <img src={goodsInfo.thumbnailUrl} />
+                            <h6>{goodsInfo.goodsName}</h6>
+                            <div>{goodsInfo.goodsPrice}</div>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
     );
 }
 
