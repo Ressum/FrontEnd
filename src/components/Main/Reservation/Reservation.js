aaab9d20da5c5b93724d00a89eb153d2ea6e19b6
kangbinkwon <kbk282655@gmail.com>
2022-11-16 00:08:57 +0900

    CHORE :: 사전예약팝업

--- src/components/Main/Reservation/Reservation.js
+++ src/components/Main/Reservation/Reservation.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './Reservation.scss';
 import { Link } from 'react-router-dom';
 import main from 'images/View/main.svg';
+import ReservationPopUp from './ReservationPopUp/ReservationPopUp';
+import { useState } from 'react';
 
 function Reservation() {
+    const [showSignUpPopUp, setShowReservationPopUp] = useState(false);
     return(
         <div id="ai-survey-result">
             <img src={main} alt="main" />
-            <button>사전예약하기</button>
+            <button onClick={()=>{setShowReservationPopUp(true);}}>사전예약하기</button>
         </div>
     );
 }
