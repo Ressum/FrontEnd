b0fbe9b35862a3c515818c5786ed20794cc7ae48
kangbinkwon <kbk282655@gmail.com>
2022-11-16 00:09:47 +0900

    사전예약 팝업

---
+++ src/components/Main/Reservation/ReservationPopUp/ReservationPopUp.js
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import './ReservationPopUp.scss';
+
+function ReservationPopUp({setShowReservationPopUp}) {
+    const navigate = useNavigate();
+
+    return (
+        <div id="sign-up-pop-up--bg">
+            <div id="sign-up-pop-up--container">
+                <h1>회원가입이 완료되었습니다!</h1>
+                <button
+                    id="sign-up-pop-up--check"
+                    onClick={() => {
+                        setShowReservationPopUp(false)
+                        navigate('/')
+                    }}
+                >확인</button>
+            </div>
+        </div>
+    )
+}
+
+export default ReservationPopUp;
