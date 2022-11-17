78aec00ce8c3fadfed06b9206294d56ecd066a24
kwonkangbin <kbk282655@gmail.com>
2022-11-17 14:41:03 +0900

    CHORE : 선택지 변경

--- src/components/AISurvey/AISurvey3/AISurvey3.js
+++ src/components/AISurvey/AISurvey3/AISurvey3.js
@@ -15,7 +15,7 @@ function AISurvey3() {
                     <input id="subject-options-1" type="checkbox" name="subject-options" />
                     <label htmlFor="subject-options-1">과학</label>
                     <input id="subject-options-2" type="checkbox" name="subject-options" />
-                    <label htmlFor="subject-options-2">의학</label>
+                    <label htmlFor="subject-options-2">병원</label>
                     <input id="subject-options-3" type="checkbox" name="subject-options" />
                     <label htmlFor="subject-options-3">영상 제작</label>
                     <input id="subject-options-4" type="checkbox" name="subject-options" />
@@ -27,13 +27,13 @@ function AISurvey3() {
                     <input id="subject-options-7" type="checkbox" name="subject-options" />
                     <label htmlFor="subject-options-7">국어</label>
                     <input id="subject-options-8" type="checkbox" name="subject-options" />
-                    <label htmlFor="subject-options-8">문학</label>
+                    <label htmlFor="subject-options-8">예체능</label>
                     <input id="subject-options-9" type="checkbox" name="subject-options" />
                     <label htmlFor="subject-options-9">음악</label>
                     <input id="subject-options-10" type="checkbox" name="subject-options" />
-                    <label htmlFor="subject-options-10">사회</label>
+                    <label htmlFor="subject-options-10">사회 문제</label>
                     <input id="subject-options-11" type="checkbox" name="subject-options" />
-                    <label htmlFor="subject-options-11">경제</label>
+                    <label htmlFor="subject-options-11">돈</label>
                     <input id="subject-options-12" type="checkbox" name="subject-options" />
                     <label htmlFor="subject-options-12">요리</label>
                 </div>
