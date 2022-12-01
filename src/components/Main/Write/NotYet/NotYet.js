2351e5c79c169b27310bd55fcb1741e250229461
kwonkangbin <kbk282655@gmail.com>
2022-12-01 22:16:37 +0900

    CHORR :: 전체적 ㅐ수정

--- src/components/Main/Write/NotYet/NotYet.js
+++ src/components/Main/Write/NotYet/NotYet.js
@@ -1,22 +1,20 @@
 import './NotYet.scss';
-import readbook from 'images/Write/read.png';
+import readbook from 'images/Write/cry.png';
 import { useNavigate } from "react-router-dom";
 
 function NotYet() {
     const navigate = useNavigate();
     return (
         <div id="not-yet-pop-up-bg">
-            <img src={readbook} alt="read"/>
+            <img id="notYet-img" src={readbook} alt="read"/>
             <div id="not-yet-pop-up">
                 <h2>준비중인<br/>페이지입니다.</h2>
-                <div id="not-yet-pop-up-buttons">
                     <button 
                         id="short-cut-button"
                         onClick={() => {
                             navigate('/');
                         }}
                     >홈으로 돌아가기</button>
-                </div>
                 <div id="not-yet-pop-up-line"></div>
             </div>
         </div>
